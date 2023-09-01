2fde43fb61196cd25ddf7e599c2d9e4f9720d616
Ozwald <37919306+0zwald@users.noreply.github.com>
2023-09-01 11:16:12 -0600

    Add files via upload

---
+++ Resolve-HostsDNS.ps1
@@ -0,0 +1,42 @@
+﻿Clear-Host
+# Define an array of server
+$remoteComputers="hostname1","hostname2"
+# Define an array of DNS addresses to resolve and ping
+$dnsAddresses = @("google.com")
+
+$dnsResults=@()
+foreach($remoteComputer in $remoteComputers){
+
+
+
+
+# Loop through each DNS address and resolve it on the remote computer
+foreach ($address in $dnsAddresses) {
+    $dnsResult = Invoke-Command -ComputerName $remoteComputer -ScriptBlock {
+        param($dnsAddress)
+        Resolve-DnsName -Name $dnsAddress
+    } -ArgumentList $address
+
+    # Display the results
+    Write-Host "DNS resolution for $address on $remoteComputer :"
+    $dnsResult | Format-Table -AutoSize
+    Write-Host ""
+
+    # Ping the DNS address from the remote computer
+    $pingResult = Invoke-Command -ComputerName $remoteComputer -ScriptBlock {
+        param($targetAddress)
+        Test-Connection -ComputerName $targetAddress -Count 4 -ErrorAction SilentlyContinue
+    } -ArgumentList $address
+
+    if ($pingResult) {
+        Write-Host "Ping results for $address from $remoteComputer :"
+        $pingResult  | Format-Table -AutoSize
+    } else {
+        Write-Host "Ping to $address from $remoteComputer failed."
+    }
+
+    Write-Host ""
+}
+
+}
+
